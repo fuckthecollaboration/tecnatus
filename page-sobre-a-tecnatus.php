2391f7913356a199640de68b241231c018d4c465
Lorena <kisarage3@gmail.com>
2020-01-23 15:54:07 -0300

    Criado página -sobre a tecnatus- que írá conter as informações da incubadora, incluindo o conceito em blocos do texto.

---
+++ page-sobre-a-tecnatus.php
@@ -0,0 +1,15 @@
+<?php /* Template Name: Sobre */ ?>
+
+<?php get_header(); ?>
+
+<div class="container container-post">
+	<hr />
+		<h1> SOBRE A TECNATUS </h1>
+	<hr />
+	
+	<p>A Tecnatus é uma incubadora de empresas que funciona como um mecanismo de estímulo e apoio ao empreendedorismo, à inovação e à geração de novos negócios. Situada no Centro de Tecnologia da Universidade Federal do Rio Grande do Norte (UFRN) na cidade de Natal, a Incubadora oferece orientação para a criação e desenvolvimento de empreendimentos inovadores de base tecnológica nas áreas de Engenharia, Química e Ciências Exatas e da Terra.</p>
+
+	<p>O programa dispõe do espaço físico e recursos tecnológicos, de uso individual e coletivo para o desenvolvimento das atividades empresariais, como também apoio gerencial e consultorias nas mais diversas áreas.</p>
+</div>
+
+<?php get_footer(); ?>
