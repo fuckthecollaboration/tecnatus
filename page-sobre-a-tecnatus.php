ee9a3bbff13d9795bbb2c32fed117c0a1403bd3a
Lorena <kisarage3@gmail.com>
2020-02-07 10:52:31 -0300

    Criado página Sobre e adicionado template para realizar alterações ao wordpress através do código.

--- page-sobre-a-tecnatus.php
+++ page-sobre-a-tecnatus.php
@@ -3,9 +3,8 @@
 <?php get_header(); ?>
 
 <div class="container container-post">
-	<hr />
-		<h1> SOBRE A TECNATUS </h1>
-	<hr />
+
+	<h1 class="page-header">Sobre a Tecnatus</h1>
 	
 	<p>A Tecnatus é uma incubadora de empresas que funciona como um mecanismo de estímulo e apoio ao empreendedorismo, à inovação e à geração de novos negócios. Situada no Centro de Tecnologia da Universidade Federal do Rio Grande do Norte (UFRN) na cidade de Natal, a Incubadora oferece orientação para a criação e desenvolvimento de empreendimentos inovadores de base tecnológica nas áreas de Engenharia, Química e Ciências Exatas e da Terra.</p>
 
