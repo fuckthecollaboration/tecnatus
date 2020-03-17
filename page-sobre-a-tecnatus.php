6d9389a9deb44f821f6a692f76d3dededd528a74
Lorena <kisarage3@gmail.com>
2020-03-17 16:47:53 -0300

    Realizadas alterações no estilo da página 'Sobre a Tecnatus' para adaptar a um novo estilo de arccordions de 2 colunas.

--- page-sobre-a-tecnatus.php
+++ page-sobre-a-tecnatus.php
@@ -3,138 +3,147 @@
 <?php get_header(); ?>
 
 <section id="page-sobre">
-<div class="container container-post">
-	<h1 class="page-header">SOBRE A TECNATUS</h1>
+	<div class="feature_background"></div>	
+	<div class="container container-post">
+		<h1 class="page-header">SOBRE A TECNATUS</h1>
 
-	<div class="accordion" id="accordionAbout"> <!-- Início arccordion main container-->
-		<div class="accordion_container">
-		    
-		    <div class="accordion_header" data-toggle="collapse" data-target="#collapse1" aria-expanded="false" aria-controls="collapse1">
-		    	<i class="fa fa-angle-double-down"></i>
-			          Sobre
-		    </div>
-			<div id="collapse1" class="collapse show" aria-labelledby="heading1" data-parent="#accordionAbout">
-			    <p>A Tecnatus é uma incubadora de empresas que funciona como um mecanismo de estímulo e apoio ao empreendedorismo, à inovação e à geração de novos negócios. Oferecemos oportunidades de desenvolvimento para empresas nascentes que buscam solucionar problemas existentes utilizando ideias inovadoras que possam gerar um impacto positivo à sociedade. Acreditamos que a inovação é o caminho para o desenvolvimento da economia e das pessoas.</p>
-			    <p>Situada no Centro de Tecnologia da Universidade Federal do Rio Grande do Norte (UFRN) na cidade de Natal, a Incubadora oferece orientação para a criação e desenvolvimento de empreendimentos inovadores de base tecnológica nas áreas de Engenharia, Química e Ciências Exatas e da Terra.</p>
-			    <p>O programa de incubação dispõe de espaço físico e recursos tecnológicos, de uso individual e coletivo para o desenvolvimento das atividades empresariais, como também apoio gerencial e consultorias nas mais diversas áreas.</p>
+		<div class="row feature_row">
+
+			<!-- Feature Content -->
+			<div class="col-lg-6 feature_col">
+				<div class="feature_content">
+					<div class="accordion" id="accordionAbout"> <!-- Início arccordion main container-->
+						<div class="accordion_container">
+						    
+						    <div class="accordion_header" data-toggle="collapse" data-target="#collapse1" aria-expanded="false" aria-controls="collapse1">
+						    	<i class="fa fa-angle-double-down"></i>
+							          Sobre
+						    </div>
+							<div id="collapse1" class="collapse show" aria-labelledby="heading1" data-parent="#accordionAbout">
+							    <p>A Tecnatus é uma incubadora de empresas que funciona como um mecanismo de estímulo e apoio ao empreendedorismo, à inovação e à geração de novos negócios. Oferecemos oportunidades de desenvolvimento para empresas nascentes que buscam solucionar problemas existentes utilizando ideias inovadoras que possam gerar um impacto positivo à sociedade. Acreditamos que a inovação é o caminho para o desenvolvimento da economia e das pessoas.</p>
+							    <p>Situada no Centro de Tecnologia da Universidade Federal do Rio Grande do Norte (UFRN) na cidade de Natal, a Incubadora oferece orientação para a criação e desenvolvimento de empreendimentos inovadores de base tecnológica nas áreas de Engenharia, Química e Ciências Exatas e da Terra.</p>
+							    <p>O programa de incubação dispõe de espaço físico e recursos tecnológicos, de uso individual e coletivo para o desenvolvimento das atividades empresariais, como também apoio gerencial e consultorias nas mais diversas áreas.</p>
+							</div>
+						</div>
+						
+						<div class="accordion_container">
+						    <div class="accordion_header" data-toggle="collapse" data-target="#collapse2" aria-expanded="true" aria-controls="collapse2">
+						    	<i class="fa fa-angle-double-down"></i>
+							          Missão
+						    </div>
+						    <div id="collapse2" class="collapse" aria-labelledby="heading2" data-parent="#accordionAbout">
+							    <p>Incentivar e promover o empreendedorismo e a inovação nas áreas de Engenharia, Arquitetura e Ciências Exatas e da Terra através da interação entre universidade, governo, empresas e sociedade em geral. Universidade Federal do Rio Grande do Norte.</p>
+						    </div>
+						</div>
+
+						<div class="accordion_container">
+						    <div class="accordion_header" data-toggle="collapse" data-target="#collapse3" aria-expanded="true" aria-controls="collapse3">
+						    	<i class="fa fa-angle-double-down"></i>
+							          Visão
+						    </div>
+						    <div id="collapse3" class="collapse" aria-labelledby="heading3" data-parent="#accordionAbout">
+							    <p>Ser referência como um ambiente de transformação de ideias em resultados de forma sustentável nas áreas de Engenharia, Arquitetura e Ciências Exatas e da Terra.</p>
+						    </div>
+						</div>
+
+						<div class="accordion_container">
+						    <div class="accordion_header" data-toggle="collapse" data-target="#collapse4" aria-expanded="true" aria-controls="collapse4">
+						    	<i class="fa fa-angle-double-down"></i>
+							          Valores
+						    </div>
+						    <div id="collapse4" class="collapse" aria-labelledby="heading4" data-parent="#accordionAbout">
+							    <p>Ética, comprometimento, respeito, responsabilidade, confiabilidade, próatividade, sustentabilidade e consciência ambiental.</p>
+						    </div>
+						</div>
+						
+						<div class="accordion_container">
+						    <div class="accordion_header" data-toggle="collapse" data-target="#collapse5" aria-expanded="true" aria-controls="collapse5">
+						    	<i class="fa fa-angle-double-down"></i>
+							          Como faço para participar?
+						    </div>
+						    <div id="collapse5" class="collapse" aria-labelledby="heading5" data-parent="#accordionAbout">
+							    <p>Após ler atentamente o edital, é necessário realizar a inscrição acessando o SIGEventos. Por meio desse sistema, é possível preencher os dados iniciais, anexar a sua proposta e gerar GRU de pagamento da taxa de inscrição. Não se esqueça de ler o manual de inscrição disponível na aba “processo seletivo aberto”, nesse documento tem o passo-a-passo que você vai precisar seguir para realizar sua inscrição.</p>
+						    </div>
+						</div>
+
+						<div class="accordion_container">
+						    <div class="accordion_header" data-toggle="collapse" data-target="#collapse6" aria-expanded="true" aria-controls="collapse6">
+						    	<i class="fa fa-angle-double-down"></i>
+							          Como sei do resultado?
+						    </div>
+						    <div id="collapse6" class="collapse" aria-labelledby="heading6" data-parent="#accordionAbout">
+							    <p>Os resultados das avaliações são publicados no site oficial da incubadora dentro dos prazos previstos no cronograma disposto em edital e, caso a sua proposta seja selecionada, a startup será convidada para participar da Banca de Seleção em data previamente agendada. Se não, você poderá reestruturar o projeto e submetê-lo para nova avaliação.</p>
+						    </div>
+						</div>
+
+						<div class="accordion_container">
+						    <div class="accordion_header" data-toggle="collapse" data-target="#collapse7" aria-expanded="true" aria-controls="collapse7">
+						    	<i class="fa fa-angle-double-down"></i>
+							          Quem pode submeter projetos?
+						    </div>
+						    <div id="collapse7" class="collapse" aria-labelledby="heading7" data-parent="#accordionAbout">
+							    <p>Alunos, egressos e empresas que possuam ideias inovadoras, interesse em novos negócios e estejam desenvolvendo soluções tecnologicamente novas ou com melhorias significativas em produtos ou processos. Se sua proposta envolver unicamente a prestação de serviços, não poderá ser admitida no Sistema de Incubação por não atender o perfil da incubadora.</p>
+						    </div>
+						</div>
+
+						<div class="accordion_container">
+						    <div class="accordion_header" data-toggle="collapse" data-target="#collapse8" aria-expanded="true" aria-controls="collapse8">
+						    	<i class="fa fa-angle-double-down"></i>
+							          Pré-incubação
+						    </div>
+						    <div id="collapse8" class="collapse" aria-labelledby="heading8" data-parent="#accordionAbout">
+							    <p>Período de 12 meses prorrogáveis por mais 6 meses. Nessa etapa, o empreendedor recebe orientação para desenvolver sua ideia desde os aspectos iniciais inerentes à constituição de uma empresa até a emissão de sua primeira Nota Fiscal (ou criação de CNPJ).</p>
+						    </div>
+						</div>
+
+						<div class="accordion_container">
+						    <div class="accordion_header" data-toggle="collapse" data-target="#collapse9" aria-expanded="true" aria-controls="collapse9">
+						    	<i class="fa fa-angle-double-down"></i>
+							          Incubação
+						    </div>
+						    <div id="collapse9" class="collapse" aria-labelledby="heading9" data-parent="#accordionAbout">
+							    <p>Período de 24 meses prorrogáveis por mais 12 meses até sua graduação. Nessa etapa, a empresa incubada recebe apoio técnico e comercial visando escalar e conquistar novos mercados.</p>
+						    </div>
+						</div>
+
+						<div class="accordion_container">
+						    <div class="accordion_header" data-toggle="collapse" data-target="#collapse10" aria-expanded="true" aria-controls="collapse10">
+						    	<i class="fa fa-angle-double-down"></i>
+							          Como funciona o programa?
+						    </div>
+						    <div id="collapse10" class="collapse" aria-labelledby="heading10" data-parent="#accordionAbout">
+							    <p>Uma vez admitidas no Sistema de Incubação, as startups serão avaliadas em seu processo de desenvolvimento. Após o diagnóstico individualizado para identificar as demandas de cada empresa é traçado um plano de ação para cada empresa com cronograma de reuniões, capacitações, mentorias, oficinas e outras tarefas. A empresa precisa ser assídua nas reuniões propostas e cumprir as deliberações estabelecidas em comum acordo nas sessões de mentorias dentro do prazo acordado.</p>
+						    </div>
+						</div>
+
+						<div class="accordion_container">
+						    <div class="accordion_header" data-toggle="collapse" data-target="#collapse11" aria-expanded="true" aria-controls="collapse11">
+						    	<i class="fa fa-angle-double-down"></i>
+							    	Quais os benefícios?
+						    </div>
+						    <div id="collapse11" class="collapse" aria-labelledby="heading11" data-parent="#accordionAbout">
+							    <p>Mentorias com empreendedores e executivos de mercado;
+									Apoio em projetos de captação de recursos nacionais e internacionais;
+									Conexão com empresas do ecossistema empreendedor universitário;
+									Apresentação para investidores;
+									Networking e troca de conhecimento;
+									Apoio em ações de visibilidade na imprensa local e nacional;
+									Participação em eventos;
+									Ambientes para realização de eventos de diferentes portes, como auditórios e salas;
+									Salas de reunião e videoconferência;
+									Capacitações, oficinas e mentorias;
+									Disseminação e sensibilização da cultura de empreendedorismo e inovação;
+									Desenvolvimento dos empreendedores enquanto líderes e aspectos de relacionamento interpessoal;
+									Conexão entre empreendedores incubados com potenciais parceiros, investidores, sócios e mentores;
+								</p>
+						    </div>
+						</div>
+
+
+					</div> <!-- Fim accordion main container -->
+				</div>
 			</div>
 		</div>
-		
-		<div class="accordion_container">
-		    <div class="accordion_header" data-toggle="collapse" data-target="#collapse2" aria-expanded="true" aria-controls="collapse2">
-		    	<i class="fa fa-angle-double-down"></i>
-			          Missão
-		    </div>
-		    <div id="collapse2" class="collapse" aria-labelledby="heading2" data-parent="#accordionAbout">
-			    <p>Incentivar e promover o empreendedorismo e a inovação nas áreas de Engenharia, Arquitetura e Ciências Exatas e da Terra através da interação entre universidade, governo, empresas e sociedade em geral. Universidade Federal do Rio Grande do Norte.</p>
-		    </div>
-		</div>
-
-		<div class="accordion_container">
-		    <div class="accordion_header" data-toggle="collapse" data-target="#collapse3" aria-expanded="true" aria-controls="collapse3">
-		    	<i class="fa fa-angle-double-down"></i>
-			          Visão
-		    </div>
-		    <div id="collapse3" class="collapse" aria-labelledby="heading3" data-parent="#accordionAbout">
-			    <p>Ser referência como um ambiente de transformação de ideias em resultados de forma sustentável nas áreas de Engenharia, Arquitetura e Ciências Exatas e da Terra.</p>
-		    </div>
-		</div>
-
-		<div class="accordion_container">
-		    <div class="accordion_header" data-toggle="collapse" data-target="#collapse4" aria-expanded="true" aria-controls="collapse4">
-		    	<i class="fa fa-angle-double-down"></i>
-			          Valores
-		    </div>
-		    <div id="collapse4" class="collapse" aria-labelledby="heading4" data-parent="#accordionAbout">
-			    <p>Ética, comprometimento, respeito, responsabilidade, confiabilidade, próatividade, sustentabilidade e consciência ambiental.</p>
-		    </div>
-		</div>
-		
-		<div class="accordion_container">
-		    <div class="accordion_header" data-toggle="collapse" data-target="#collapse5" aria-expanded="true" aria-controls="collapse5">
-		    	<i class="fa fa-angle-double-down"></i>
-			          Como faço para participar?
-		    </div>
-		    <div id="collapse5" class="collapse" aria-labelledby="heading5" data-parent="#accordionAbout">
-			    <p>Após ler atentamente o edital, é necessário realizar a inscrição acessando o SIGEventos. Por meio desse sistema, é possível preencher os dados iniciais, anexar a sua proposta e gerar GRU de pagamento da taxa de inscrição. Não se esqueça de ler o manual de inscrição disponível na aba “processo seletivo aberto”, nesse documento tem o passo-a-passo que você vai precisar seguir para realizar sua inscrição.</p>
-		    </div>
-		</div>
-
-		<div class="accordion_container">
-		    <div class="accordion_header" data-toggle="collapse" data-target="#collapse6" aria-expanded="true" aria-controls="collapse6">
-		    	<i class="fa fa-angle-double-down"></i>
-			          Como sei do resultado?
-		    </div>
-		    <div id="collapse6" class="collapse" aria-labelledby="heading6" data-parent="#accordionAbout">
-			    <p>Os resultados das avaliações são publicados no site oficial da incubadora dentro dos prazos previstos no cronograma disposto em edital e, caso a sua proposta seja selecionada, a startup será convidada para participar da Banca de Seleção em data previamente agendada. Se não, você poderá reestruturar o projeto e submetê-lo para nova avaliação.</p>
-		    </div>
-		</div>
-
-		<div class="accordion_container">
-		    <div class="accordion_header" data-toggle="collapse" data-target="#collapse7" aria-expanded="true" aria-controls="collapse7">
-		    	<i class="fa fa-angle-double-down"></i>
-			          Quem pode submeter projetos?
-		    </div>
-		    <div id="collapse7" class="collapse" aria-labelledby="heading7" data-parent="#accordionAbout">
-			    <p>Alunos, egressos e empresas que possuam ideias inovadoras, interesse em novos negócios e estejam desenvolvendo soluções tecnologicamente novas ou com melhorias significativas em produtos ou processos. Se sua proposta envolver unicamente a prestação de serviços, não poderá ser admitida no Sistema de Incubação por não atender o perfil da incubadora.</p>
-		    </div>
-		</div>
-
-		<div class="accordion_container">
-		    <div class="accordion_header" data-toggle="collapse" data-target="#collapse8" aria-expanded="true" aria-controls="collapse8">
-		    	<i class="fa fa-angle-double-down"></i>
-			          Pré-incubação
-		    </div>
-		    <div id="collapse8" class="collapse" aria-labelledby="heading8" data-parent="#accordionAbout">
-			    <p>Período de 12 meses prorrogáveis por mais 6 meses. Nessa etapa, o empreendedor recebe orientação para desenvolver sua ideia desde os aspectos iniciais inerentes à constituição de uma empresa até a emissão de sua primeira Nota Fiscal (ou criação de CNPJ).</p>
-		    </div>
-		</div>
-
-		<div class="accordion_container">
-		    <div class="accordion_header" data-toggle="collapse" data-target="#collapse9" aria-expanded="true" aria-controls="collapse9">
-		    	<i class="fa fa-angle-double-down"></i>
-			          Incubação
-		    </div>
-		    <div id="collapse9" class="collapse" aria-labelledby="heading9" data-parent="#accordionAbout">
-			    <p>Período de 24 meses prorrogáveis por mais 12 meses até sua graduação. Nessa etapa, a empresa incubada recebe apoio técnico e comercial visando escalar e conquistar novos mercados.</p>
-		    </div>
-		</div>
-
-		<div class="accordion_container">
-		    <div class="accordion_header" data-toggle="collapse" data-target="#collapse10" aria-expanded="true" aria-controls="collapse10">
-		    	<i class="fa fa-angle-double-down"></i>
-			          Como funciona o programa?
-		    </div>
-		    <div id="collapse10" class="collapse" aria-labelledby="heading10" data-parent="#accordionAbout">
-			    <p>Uma vez admitidas no Sistema de Incubação, as startups serão avaliadas em seu processo de desenvolvimento. Após o diagnóstico individualizado para identificar as demandas de cada empresa é traçado um plano de ação para cada empresa com cronograma de reuniões, capacitações, mentorias, oficinas e outras tarefas. A empresa precisa ser assídua nas reuniões propostas e cumprir as deliberações estabelecidas em comum acordo nas sessões de mentorias dentro do prazo acordado.</p>
-		    </div>
-		</div>
-
-		<div class="accordion_container">
-		    <div class="accordion_header" data-toggle="collapse" data-target="#collapse11" aria-expanded="true" aria-controls="collapse11">
-		    	<i class="fa fa-angle-double-down"></i>
-			    	Quais os benefícios?
-		    </div>
-		    <div id="collapse11" class="collapse" aria-labelledby="heading11" data-parent="#accordionAbout">
-			    <p>Mentorias com empreendedores e executivos de mercado;
-					Apoio em projetos de captação de recursos nacionais e internacionais;
-					Conexão com empresas do ecossistema empreendedor universitário;
-					Apresentação para investidores;
-					Networking e troca de conhecimento;
-					Apoio em ações de visibilidade na imprensa local e nacional;
-					Participação em eventos;
-					Ambientes para realização de eventos de diferentes portes, como auditórios e salas;
-					Salas de reunião e videoconferência;
-					Capacitações, oficinas e mentorias;
-					Disseminação e sensibilização da cultura de empreendedorismo e inovação;
-					Desenvolvimento dos empreendedores enquanto líderes e aspectos de relacionamento interpessoal;
-					Conexão entre empreendedores incubados com potenciais parceiros, investidores, sócios e mentores;
-				</p>
-		    </div>
-		</div>
-
-
-	</div> <!-- Fim accordion main container -->
-</div>
+	</div>
 </section>
 <?php get_footer(); ?>
